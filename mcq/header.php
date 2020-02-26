8f64c89660af7f1bef61639218843d60a151a6e1
sagar yadav <40927722+rahulyadav97185966@users.noreply.github.com>
2020-02-26 11:55:23 +0530

    Add files via upload

---
+++ mcq/header.php
@@ -0,0 +1,35 @@
+ <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="../index.php">Lastnotes</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+         <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../index.php#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../index.php#about">Courses</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../index.php#service">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../index.php#team">Team</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../index.php#contact">Contact</a>
+          </li>
+          
+          <!-- <li class="nav-item">
+            <a class="nav-link js-scroll" href="#blog">Blog</a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ mcq/subjects.php
@@ -0,0 +1,66 @@
+ <div class="col-md-4">
+           <div class="widget-sidebar widget-tags">
+            <h5 class="sidebar-title">Subjects</h5>
+            <div class="sidebar-content">
+              <ul>
+                <li>
+                  <a href="m3.php">M-3</a>
+                </li>
+                <li>
+                  <a href="cao.php">CAO</a>
+                </li>
+                <li>
+                  <a href="#">MPMC</a>
+                </li>
+                <li>
+                  <a href="stld.php">STLD</a>
+                </li>
+                <li>
+                  <a href="cpp.php">C++</a>
+                </li>
+                <li>
+                  <a href="java.php">Java</a>
+                </li>
+                <li>
+                  <a href="#">Web</a>
+                </li>
+                <li>
+                  <a href="#">DS</a>
+                </li>
+                <li>
+                  <a href="#">INP</a>
+                </li>
+                <li>
+                  <a href="#">DSA</a>
+                </li>
+                <li>
+                  <a href="pde.php">PDE</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+            <div class="widget-sidebar">
+            <h5 class="sidebar-title">What We Offers</h5>
+            <div class="sidebar-content">
+              <ul class="list-sidebar">
+                <li>
+                  <a href="#">LASTMINUTE NOTES.</a>
+                </li>
+                <li>
+                  <a href="#">LAB MANUALS</a>
+                </li>
+                <li>
+                  <a href="#">COLLEGE NOTES</a>
+                </li>
+                <li>
+                  <a href="#">UNIVERSITY PAPERS</a>
+                </li>
+                <li>
+                  <a href="#">BOOKS</a>
+                </li>
+                <li>
+                  <a href="#">VERIFIED NOTES</a>
+                </li>
+              </ul>
+            </div>
+          </div>
